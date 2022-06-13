0a0e635b59511d3a0c6f6eda4290380fecbcb6a1
koalita-corsica <jiann@koalita.corsica>
2022-06-13 22:59:17 +0200

    api

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -10,7 +10,6 @@ import Layout from "../containers/layout";
 import { Link } from "gatsby";
 
 
-import { BsFillPlusSquareFill } from "react-icons/bs";
 
 import * as styles from "../styles/home.css";
 
@@ -58,49 +57,48 @@ const IndexPage = (props) => {
     //  log("hey : " + filtered.dataset.entreprise);
 
   }
-  function createCompany() {
-    axios.get('https://api.dev.evrpro.com/societes/', {
-      headers: {
-          'Authorization' : 'Bearer 3|kHg1Af40ugAHycMm1kJsFdZp2jchfYuioIwcMyNs',
-          'Content-Type' : 'application/json',
-          'Accept' : 'application/json',
-      }
-    })
-    .then(function (response) {
-        // handle success
-        for(var i in response.data.data){
+  //function createCompany() {
+    // axios.get('https://api.dev.evrpro.com/societes/', {
+    //   headers: {
+    //       'Authorization' : 'Bearer 3|kHg1Af40ugAHycMm1kJsFdZp2jchfYuioIwcMyNs',
+    //       'Content-Type' : 'application/json',
+    //       'Accept' : 'application/json',
+    //   }
+    // })
+    // .then(function (response) {
+    //     // handle success
+    //     for(var i in response.data.data){
         
-            const doc = {
-                _type: 'company',
-                title: response.data.data[i].raison_sociale,
-                statut: response.data.data[i].statut_juridique,
-                gerant: response.data.data[i].gerants.name,
-                activite: response.data.data[i].activite.activite,
-                code: response.data.data[i].activite.code,
-                division: response.data.data[i].activite.division,
-                code_postal: response.data.data[i].adresse.code_postal,
-                rue: response.data.data[i].adresse.rue,
-                ville: response.data.data[i].adresse.ville,
-                email: response.data.data[i].contact.email,
-                fax: response.data.data[i].contact.fax,
-                telephone: response.data.data[i].contact.telephone
-            }
+    //         const doc = {
+    //             _type: 'company',
+    //             title: response.data.data[i].raison_sociale,
+    //             statut: response.data.data[i].statut_juridique,
+    //             gerant: response.data.data[i].gerants.name,
+    //             activite: response.data.data[i].activite.activite,
+    //             code: response.data.data[i].activite.code,
+    //             division: response.data.data[i].activite.division,
+    //             code_postal: response.data.data[i].adresse.code_postal,
+    //             rue: response.data.data[i].adresse.rue,
+    //             ville: response.data.data[i].adresse.ville,
+    //             email: response.data.data[i].contact.email,
+    //             fax: response.data.data[i].contact.fax,
+    //             telephone: response.data.data[i].contact.telephone
+    //         }
             
-            console.log(doc)
-            // client.create(doc)
-        }
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
-  }
+    //         console.log(doc)
+    //         client.create(doc)
+    //     }
+    // })
+    // .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    // });
+  //}
 
 
   return (
         <Layout>
           <label>Filtrer les entreprises : <input type="text" onKeyUp={recherche} /></label>
-          <button onClick={createCompany}>Api test</button>
           <div data-homeWrapper >
             <div data-gridHome>              
               {company.map((item, i) =>
